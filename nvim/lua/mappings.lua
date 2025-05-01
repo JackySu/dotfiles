4880a8b510696acf67c92669f0ac2fc4a2761afa
iulx0 <sus1@tcd.ie>
2025-05-01 16:31:22 +0800

    add <Leader>ca to apply code action (quick fix etc.)

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -27,3 +27,5 @@ map("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "De
 
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<Leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { desc = "Apply code action" })
