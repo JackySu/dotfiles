a6ca6bee726f39c3b68b734b33867d17864bb4c9
sushi <shis@icbench.com>
2025-10-09 15:25:29 +0800

    add copilot plugin and update lock file

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -83,4 +83,25 @@ return {
   -- 		},
   -- 	},
   -- },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    lazy = false,
+    enabled = true,
+    opts = {
+      panel = {
+        auto_refresh = true,
+        layout = {
+          position = "right",
+          ratio = 0.3,
+        },
+      },
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-l>",
+        },
+      },
+    },
+  },
 }
