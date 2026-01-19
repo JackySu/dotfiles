d9e94d0e2a74bb4117393f20891b6071d84d498b
iulx0 <sus1@tcd.ie>
2026-01-19 23:41:48 +0800

    update: replace copilot.lua with minuet.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -288,25 +288,43 @@ return {
   },
 
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    lazy = false,
-    enabled = true,
-    opts = {
-      panel = {
-        auto_refresh = true,
-        layout = {
-          position = "right",
-          ratio = 0.3,
+    "milanglacier/minuet-ai.nvim",
+    config = function()
+      require("minuet").setup({
+        provider = "openai_fim_compatible",
+        provider_options = {
+          openai_fim_compatible = {
+            model = "deepseek-chat",
+            endpoint = "https://api.deepseek.com/beta/completions",
+            api_key = "DEEPSEEK_API_KEY",
+            name = "deepseek",
+            stream = true,
+            optional = {
+              max_tokens = 256,
+              stop = { "\n\n" },
+              top_p = 0.9,
+            },
+          },
         },
-      },
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-l>",
+        virtualtext = {
+          auto_trigger_ft = { "*" },
+          keymap = {
+            -- accept whole completion
+            accept = "<A-A>",
+            -- accept one line
+            accept_line = "<A-a>",
+            -- accept n lines (prompts for number)
+            -- e.g. "A-z 2 CR" will accept 2 lines
+            accept_n_lines = "<A-z>",
+            -- Cycle to prev completion item, or manually invoke completion
+            prev = "<A-[>",
+            -- Cycle to next completion item, or manually invoke completion
+            next = "<A-]>",
+            dismiss = "<A-e>",
+          },
         },
-      },
-    },
+      })
+    end,
   },
 
   {
